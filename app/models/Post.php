2221e43e61ba7fa3cfc1ae70be184c017aee1851
Ovsi <98219774+Ovsi@users.noreply.github.com>
2022-02-02 22:26:24 +0400

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -20,4 +20,15 @@ class Post
     public function store($store){
 
     }
+    
+    //stexcel em function tvyalnnery popoxelu hamar ev argumentov sahmanel  em ayn tvyalnnery voronq petqa poxancven
+    public function updateData($title,$content,$user_id){
+        //$this obyekti operatori mijocov hasaneliutyun em unecel tvyalneri bazayin
+        $db=$this->connect();
+        //mysql update hramani mijocov sahmanel em ayn tvyalnery voronq petqa popoxven ev pahpanel em dranq update popoxakani mej
+        $update="UPDATE `posts` SET `id`=Null, `title`='$title', `content`='$content', `published_at`=NOW(), `user_id`='$user_id' WHERE `user_id`=$user_id";
+        //katarel em update popoxakani mej pahvac harcumy
+        $result=$this->db->query($update);
+    }
+
 }
